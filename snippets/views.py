c0bda3f6a70d5d3a393d403c77429b817a340a97
yangxu <zhyu@smail.nju.edu.cn>
2019-07-31 16:56:04 +0800

    modified Tutorial 3:Class-based Views using mixins

--- snippets/views.py
+++ snippets/views.py
@@ -3,11 +3,6 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
 
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
 
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
@@ -37,27 +32,3 @@ class SnippetDetail(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
-
-    def get_object(selfself, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
